fa6fe8ebac198fce0bed9f52b2720f6fbdc02870
attilakoksal <attila.b.koksal@gmail.com>
2025-06-05 16:39:25 -0700

    Added Part B search engine implementation and indexer

---
+++ app.py
@@ -0,0 +1,109 @@
+## In the terminal, "export FLASK_APP=flask_demo" (without .py)
+## flask run -h 0.0.0.0 -p 8888
+
+import lucene
+import os
+from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
+from java.nio.file import Paths
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions, DirectoryReader
+from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
+from org.apache.lucene.search.similarities import BM25Similarity
+from flask import request, Flask, render_template
+
+import math
+from datetime import datetime
+
+app = Flask(__name__)
+
+def retrieve(storedir, query):
+
+    searchDir = NIOFSDirectory(Paths.get(storedir))
+    searcher = IndexSearcher(DirectoryReader.open(searchDir))
+    
+    parser = QueryParser('title', StandardAnalyzer())
+    parsed_query = parser.parse(query)
+
+    topDocs = searcher.search(parsed_query, 10).scoreDocs
+    topkdocs = []
+    for hit in topDocs:
+        doc = searcher.doc(hit.doc)
+        print(doc.get("author"))
+        print(doc.get("ratio"))
+        topkdocs.append({
+            "score": hit.score,
+            "title": doc.get("title"),
+            "subreddit": doc.get("subreddit"),
+            "upvotes": doc.get("upvotes"),
+            "time": doc.get("time"),
+            "author": doc.get("author"),
+            "ratio": doc.get("ratio"),
+            "ID": doc.get("ID")
+        })
+    return topkdocs
+    #print(topkdocs)
+def relevance_score(upvotes, downvotes, created_time_str):
+    score = upvotes - downvotes
+    score *= math.log(max(1, abs(score)))
+    created_time = datetime.strptime(created_time_str, '%Y-%m-%d %H:%M:%S')
+    beginning = datetime(1970,1,1)
+    age = (created_time - beginning).total_seconds()
+    age /= 45000
+    return score + age
+
+@app.route("/")
+def home():
+    return render_template('input.html')
+
+@app.route('/input', methods = ['POST', 'GET'])
+def input():
+    return render_template('input.html')
+
+@app.route('/output', methods = ['POST', 'GET'])
+def output():
+    if request.method == 'GET':
+        return f"Nothing"
+    if request.method == 'POST':
+        form_data = request.form
+        query = form_data['query'].strip()
+        if not query:
+            return render_template('output.html', error="Please enter a query.")
+        sort_by = form_data.get('filter_option', 'Relevant')
+        #print(f"this is the query: {query}")
+        lucene.getVMEnv().attachCurrentThread()
+        docs = retrieve('index/', str(query))
+        #print(docs)
+        print(sort_by)
+        if sort_by == 'Lucene':
+            pass
+        elif sort_by == 'Relevant':
+            for doc in docs:
+                up = int(doc.get('upvotes'))
+                down = int(doc.get('upvotes')) / float(doc.get('ratio'))
+                time = doc.get('time')
+                doc['relevance_score'] = relevance_score(up, down, time)
+            docs.sort(key=lambda x: x.get('relevance_score'), reverse=True)
+
+        elif sort_by == "Newest":
+            docs.sort(key=lambda x: x.get('time'), reverse=True)
+        elif sort_by == "Oldest":
+            docs.sort(key=lambda x: x.get('time'))
+        elif sort_by == "Most_Upvoted":
+            docs.sort(key=lambda x: int(x.get('upvotes')), reverse=True)
+        elif sort_by == "Controversial":
+            docs.sort(key=lambda x: x.get('ratio'))
+
+        return render_template('output.html',query_output = query,lucene_output = docs)
+    
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# create_index('sample_lucene_index/')
+# retrieve('sample_lucene_index/', 'web data')
+
+
---
+++ index.py
@@ -0,0 +1,110 @@
+import lucene
+import os
+from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
+from java.nio.file import Paths
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType, TextField, StringField, IntPoint, FloatPoint, StoredField
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions, DirectoryReader
+from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
+from org.apache.lucene.search.similarities import BM25Similarity
+from flask import request, Flask, render_template
+
+from glob import glob
+from datetime import datetime
+import json 
+index_dir = "./index"
+data_dir = "./Reddit_Data"
+
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+store = SimpleFSDirectory(Paths.get(index_dir))
+analyzer = StandardAnalyzer()
+config = IndexWriterConfig(analyzer)
+config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+writer = IndexWriter(store, config)
+
+
+
+
+def index_reddit(post):
+    doc = Document()
+
+    #stored not tokenized
+    stringFieldType = FieldType()
+    stringFieldType.setStored(True)
+    stringFieldType.setTokenized(False)
+
+    #tokenized and stored
+    textFieldType = FieldType()
+    textFieldType.setStored(True)
+    textFieldType.setTokenized(True)
+    
+
+    doc.add(StringField("subreddit", post.get("subreddit"), Field.Store.YES))
+    doc.add(StringField("author", post.get("author"), Field.Store.YES))
+    doc.add(StringField("from", post.get("retrievedfrom"), Field.Store.YES))
+    doc.add(StringField("ID", post.get("ID"), Field.Store.YES))
+
+    doc.add(IntPoint("upvotes", int(post.get("upvotes"))))
+    doc.add(StoredField("upvotes", int(post.get("upvotes"))))
+
+    doc.add(FloatPoint("ratio", post.get("upvote_ratio")))
+    doc.add(StoredField("ratio", post.get("upvote_ratio")))
+    
+    unix_time = int(post.get("time"))
+    time_str = datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
+    doc.add(StringField("time", time_str, Field.Store.YES))
+
+    doc.add(TextField("title", post.get("title"), Field.Store.YES))
+    doc.add(TextField("body", post.get("body"), Field.Store.NO))
+
+    comments = post.get("comments")
+    for comment in comments:
+        doc.add(TextField("comment", comment, Field.Store.NO))
+
+    url_titles = post.get("url_title")
+    if url_titles and isinstance(url_titles,list):
+        for url_title in url_titles:
+            doc.add(TextField("url_title", url_title, Field.Store.YES))
+
+    writer.addDocument(doc)
+
+def crawled_links_index(post):
+    doc = Document()
+    title = post.get("title")
+    if title:
+        doc.add(TextField("link_title", title, Field.Store.YES))
+
+
+    body = post.get("body")
+    for text in body:
+        doc.add(TextField("link_body", text, Field.Store.NO))
+    doc.add(StringField("from_reddit", post.get("from_reddit", ""), Field.Store.YES))
+
+    writer.addDocument(doc)
+
+
+def main():
+    print(lucene.VERSION)
+    if not os.path.exists(index_dir):
+        os.mkdir(index_dir)
+    reddit_data = sorted(glob(os.path.join(data_dir, "chunk_*.json")))
+    reddit_data += sorted(glob(os.path.join(data_dir, "data_*.json")))
+    crawled_data = sorted(glob(os.path.join(data_dir, "crawled_links_*.json")))
+    for data in reddit_data:
+        with open(data, 'r', encoding='utf-8') as f:
+            submissions = json.load(f)
+        for submission in submissions:
+            index_reddit(submission)
+
+    for data in crawled_data:
+        with open(data, 'r', encoding='utf-8') as f:
+            links = json.load(f)
+        for link in links:
+            crawled_links_index(link)
+
+    writer.close()
+
+if __name__ == "__main__":
+    main()
