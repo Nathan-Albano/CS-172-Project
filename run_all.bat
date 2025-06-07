021361832b7fe4152e8b23eed9fd1cf251f52688
attilakoksal <attila.b.koksal@gmail.com>
2025-06-06 20:25:38 -0700

    made the bat file

---
+++ run_all.bat
@@ -0,0 +1,4 @@
+@echo off
+python3 index.py
+python3 app.py
+pause
---
+++ run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Running index.py..."
+python3 index.py
+
+echo "Starting Flask app (app.py)..."
+python3 app.py
